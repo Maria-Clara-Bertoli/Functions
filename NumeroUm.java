0788242ab12d43a696a8ceb45a31bfe46ef3a016
Maria-Clara-Bertoli <mariaclarabertoli@outlook.com>
2022-09-26 07:57:31 -0300

    Desenvolvimentos com funções
    Último commit

--- NumeroUm.java
+++ NumeroUm.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class NumeroUm {
 
 	public static void main(String[] args) {
-		
+
 		Scanner valor = new Scanner(System.in);
 		Funcao function = new Funcao();
-		
+
 		System.out.println("Informe o tamanho da saída: ");
 		int tamanho = valor.nextInt();
 		System.out.println(function.ImprimeLinhaHorizontal(tamanho));
-		
+
 		valor.close();
 	}
 
